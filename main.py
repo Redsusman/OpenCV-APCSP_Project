c3679cc521d70b9489f1889eda962a22cf453752
Jio Sookhai <jiovannizsook@grotonk12.org>
2023-04-06 14:42:04 -0700

    progress

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 import cv2
+import numpy as np
+
+img = cv2.imread('OpenCV-APCSP_Project/assets/image.jpg', cv2.IMREAD_ANYCOLOR)
+cv2.imshow("image", img)
+cv2.waitKey(3500)
+cv2.destroyAllWindows()
 
 
 
