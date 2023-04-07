b40a44e958030e7ea938453b5961b6daa3464786
Jio Sookhai <jiovannizsook@grotonk12.org>
2023-04-07 07:13:18 -0700

    working on face detection

--- main.py
+++ main.py
@@ -1,10 +1,29 @@
 import cv2
 import numpy as np
 
-img = cv2.imread('OpenCV-APCSP_Project/assets/image.jpg', cv2.IMREAD_ANYCOLOR)
-cv2.imshow("image", img)
-cv2.waitKey(3500)
+cap = cv2.VideoCapture(0)
+face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+
+
+
+
+while True:
+    
+    _,  frame = cap.read()
+    convert = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    face = face_cascade.detectMultiScale(convert, 1.1, 4)
+
+    for(x, y, z, c,) in face:
+        cv2.rectangle(frame, x, c, (0, 255, 0), 1.1, -1, 0)
+
+    cv2.imshow("Frame", frame)
+    if cv2.waitKey(1) == ord('q'):
+        break
+
+
+cap.release()
 cv2.destroyAllWindows()
+    
 
 
 
