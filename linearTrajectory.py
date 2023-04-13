c8f847468125b9d4b0a441378109c8056462f689
Jio Sookhai <jiovannizsook@grotonk12.org>
2023-04-13 06:53:05 -0700

    traject

--- linearTrajectory.py
+++ linearTrajectory.py
@@ -1,5 +1,12 @@
 import matplotlib.pyplot as plot
 import numpy as np
+import math
 
+points = np.array([(0,0), (3,2)])
 def generateLinearTrajectory(points, speed):
     return
+
+
+def generateSplineTrajectory(points, speed):
+    return
+
