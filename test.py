432a9b604b827ec715660fa753ab141c229340d6
Jio Sookhai <jiovannizsook@grotonk12.org>
2023-04-17 07:52:43 -0700

    removed

--- test.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-x_list = np.array([30, 35, 50, 70, 90], dtype=np.float32)
-y_list = np.array([5, 5, 3.6, 3.6, 1.4], dtype=np.float32)
-
-a, b, c = np.polyfit(x_list, y_list, 2)
-
-print(a, b, c)
