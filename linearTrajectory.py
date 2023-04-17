af082e5dfd7481fb5be97feeb2d8ae9c0a301a33
Jio Sookhai <jiovannizsook@grotonk12.org>
2023-04-16 18:23:36 -0700

    deletion

--- linearTrajectory.py
+++ linearTrajectory.py
@@ -4,16 +4,8 @@ import math
 
 # generate a linear trajectory from a set of points using polynomial regression
 # specify the speed to drive along the trajectory
-array = np.array([(1, 3), (5, 7)])
-def generateLinearTrajectory(points):
-    x, y = zip(*points)
-    a, b = np.polyfit(x, y, 1)
-    theta = np.degrees(np.arctan(a))
-    secondTheta = 90 - theta
-    print(a, "x +", b)
-    return a, b, theta, secondTheta
 
-def generateLinearTrajectoryComplex(points):
+def generateLinearTrajectory(points):
     secondList = points
     x = [secondList[i][0] for i in range(len(secondList))]
     y = [points[i][1] for i in range(len(points))]
@@ -23,7 +15,7 @@ def generateLinearTrajectoryComplex(points):
     print(a,"x+", b)
     return a,b,theta,secondTheta
 
-generateLinearTrajectory(array)
+
 
 #generate parabolic trajectories from an acceleration value, speeds are automatically calculated
 def generateSplineTrajectory(points, speed):
