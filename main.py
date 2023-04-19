de08cbb2fec73560564fbe81154db6efcbd50b6d
Jio Sookhai <jiovannizsook@grotonk12.org>
2023-04-19 09:04:57 -0700

    state transisition matrix with dynamics

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import cameraCalibration as calib
 while True:
        if key.is_pressed('u'):
            detectCube.run()
-           break
+           breaku
        elif key.is_pressed('o'):
            detectCone.run()
            break
