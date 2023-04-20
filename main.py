dc42920dfb4a0e4ac7781cd21186dd3c3313fa99
Jio Sookhai <jiovannizsook@grotonk12.org>
2023-04-20 11:15:39 -0700

    change

--- main.py
+++ main.py
@@ -2,15 +2,21 @@ import keyboard as key
 import detectCone
 import detectCube
 import cameraCalibration as calib
+import linearTrajectory as lt
 
 
 while True:
        if key.is_pressed('u'):
            detectCube.run()
+           while True:
+                n = 0
            break
        elif key.is_pressed('o'):
            detectCone.run()
            break
+       
+lt.generateLinearTrajectory()
+
 
 
 
