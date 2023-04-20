baffcefcbb8a5537b6fe8e886d90232f4ec2fe5d
Jio Sookhai <jiovannizsook@grotonk12.org>
2023-04-19 18:07:21 -0700

    removed

--- test.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-
-array = [1,2,3,4,5,6,7,8,9,10]
-
-for i in range(0, len(array)):
-    print(array[i], array[i+1])
-    
--- test2.py
+++
@@ -1,5 +0,0 @@
-import numpy as np
-
-array = np.array([1, 2, 3])
-np.append(array, [2, 3, 5])
-print(array)
