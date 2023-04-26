3ab2a910333af1909611465a43d780194cd954a2
Jio Sookhai <jiovannizsook@grotonk12.org>
2023-04-26 11:16:00 -0700

    graph works

--- linearTrajectory.py
+++ linearTrajectory.py
@@ -5,7 +5,7 @@ from matplotlib.animation import FuncAnimation
 import numpy.linalg as lin
 
 # generate a linear trajectory from a set of points using polynomial regression
-# returns coefficents of the function of points, and the angle of the line relative to the postive
+# returns coefficents of the linear function of points, and the angle of the line relative to the postive
 # x, and positive y axes. All these form a 1d tuple of (coefficent1, coefficent 2, theta1, theta).
 # where coefficents1 and 2 represents the equation y=mx+b, m is coefficent1, b is coefficent2.
 
@@ -18,7 +18,7 @@ def generateLinearTrajectory(points):
     secondTheta = 90 - theta
     return a,b,theta,secondTheta
 
-
+# simulate drivetrain moving towards a point using matplotlib graph
 def draw(coefficents, points):
     yList = []
     x_points = np.linspace(points[0][0], points[1][0], 10)
@@ -32,16 +32,16 @@ def draw(coefficents, points):
     ax.set_xlim(0, 250)
     ax.set_ylim(0, 100)
 
-    line = ax.plot(points[0][0], points[0][1])
+    line = ax.plot(points[0][0], points[0][1])[0]
 
     line.set_xdata(x_points)
     line.set_ydata(yList)
     
-    animation = plot.plot(x_points, yList)
-    anim = FuncAnimation(fig, func=line, frames=np.arange(0, 10, 0.01),  interval=10)
+    # animation = plot.plot(x_points, yList)
+    anim = FuncAnimation(fig, func=line.set_data, frames=np.arange(0, 10, 0.01),  interval=10)
     plot.show()
 
-points = np.array([(0,0), (13, 17)], dtype=np.float32)
+points = np.array([(0,0), (13, 6)], dtype=np.float32)
 
 coefficents = generateLinearTrajectory(points)
 
